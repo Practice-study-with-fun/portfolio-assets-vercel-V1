d5c5dc0e02030d11f22b62e2343bcd6cd9a9d2ab
Bipul <surjosharma8888@gmail.com>
2025-10-04 13:55:33 +0600

    all done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import logoimage from "../../public/logo.png";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-center text-5xl text-white">This is a portfolio assets Website </h1>
+      <div className="logoimage flex justify-center mt-10">
+        <Image src={logoimage} alt="Logo" width={200} height={200} />
+      </div>
     </div>
   );
 }
